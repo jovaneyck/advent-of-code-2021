aab48190bdf028b06c971c7661c92f83b33e64e7
Van Eyck Jo <jo.vaneyck@gmail.com>
2021-11-22 12:57:52 +0100

    cleanup

--- day 01/part1.fsx
+++ day 01/part1.fsx
@@ -1,9 +1,11 @@
-// #r "nuget: Unquote"
-// open Swensen.Unquote
+(*
+#r "nuget: Unquote"
+open Swensen.Unquote
 
-// printf "Testing..."
-// test <@ 1 + 1 = 3 @>
-// printfn "...done!"
+printf "Testing..."
+test <@ 1 + 1 = 3 @>
+printfn "...done!"
+*)
 
 let input =
-    System.IO.File.ReadAllLines($"{__SOURCE_DIRECTORY__}\input.txt")
+    System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}\input.txt"
