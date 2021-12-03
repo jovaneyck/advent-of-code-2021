8c051e34cae19c2a9582e830e4953043ba9a6c8d
Van Eyck Jo <jo.vaneyck@gmail.com>
2021-12-03 07:20:25 +0100

    update template with a string trim so we can format our example input a bit better

--- day 00/part1.fsx
+++ day 00/part1.fsx
@@ -1,6 +1,9 @@
 let input =
     System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}\input.txt"
 
+let example =
+    @"".Split("\n") |> Array.map (fun s -> s.Trim())
+
 #r "nuget: Unquote"
 open Swensen.Unquote
 
