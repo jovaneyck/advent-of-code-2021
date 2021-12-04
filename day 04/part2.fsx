d8043e28dff174ef63396d212145a6e88b41fe8b
Van Eyck Jo <jo.vaneyck@gmail.com>
2021-12-04 11:23:54 +0100

    refactor to array2D + sane parsing, better but same bug :)

--- day 04/part2.fsx
+++ day 04/part2.fsx
@@ -1,8 +1,9 @@
 let input =
-    System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}\input.txt"
+    System.IO.File.ReadAllText $"{__SOURCE_DIRECTORY__}\input.txt"
+    |> fun s -> s.Split("\r\n\r\n")
 
 let example =
-    @"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+    "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
     22 13 17 11  0
      8  2 23  4 24
@@ -21,79 +22,60 @@ let example =
     18  8 23 26 20
     22 11 13  6  5
      2  0 12  3  7"
-        .Split("\n")
-    |> Array.map (fun s -> s.Trim())
+        .Split("\n\n")
 
-type Position = { row: int; col: int }
 type Number = { value: int; marked: bool }
 
-let rec parseBoards (boardLines: string []) =
-    if boardLines |> Seq.isEmpty then
-        []
-    else
-        let board =
-            [ for (row, line) in boardLines |> Array.take 5 |> Array.indexed do
-                  let columns =
-                      line.Split(" ")
-                      |> Array.except [ "" ]
-                      |> Array.map int
-                      |> Array.indexed
-
-                  for (column, number) in columns -> ({ row = row; col = column }, { value = number; marked = false }) ]
-            |> Map.ofSeq
-
-        let rest =
-            boardLines
-            |> Array.skip (
-                if boardLines |> Seq.length > 5 then
-                    6
-                else
-                    5
-            )
-
-        board :: (parseBoards rest)
+let parseBoard (text: string) =
+    [ for line in text.Split("\n") do
+          [ for number in
+                line
+                    .Trim()
+                    .Split(" ", System.StringSplitOptions.RemoveEmptyEntries) -> { value = int number; marked = false } ] ]
+    |> array2D
 
 let parse (text: string []) =
+    let blocks = text
+
     let numbers =
-        text.[0].Split(",")
+        blocks
+        |> Seq.head
+        |> fun s -> s.Split(",")
         |> Array.map int
         |> Array.toList
 
-    let boards = parseBoards (text |> Array.skip 2)
+    let boards =
+        blocks
+        |> Seq.tail
+        |> Seq.map parseBoard
+        |> Seq.toList
+
     numbers, boards
 
 let mark numberToMark board =
     board
-    |> Map.map
-        (fun _ number ->
+    |> Array2D.map
+        (fun number ->
             if number.value = numberToMark then
                 { number with marked = true }
             else
                 number)
 
-let isWinner board =
-    let rows =
-        [ for row in 0 .. 4 ->
-              board
-              |> Map.toList
-              |> List.filter (fun (pos, _) -> pos.row = row) ]
-
-    let cols =
-        [ for col in 0 .. 4 ->
-              board
-              |> Map.toList
-              |> List.filter (fun (pos, _) -> pos.col = col) ]
-
+let isWinner (board: Number [,]) =
+    let rows = [ for i in 0 .. 4 -> board.[i, *] ]
+    let cols = [ for i in 0 .. 4 -> board.[*, i] ]
 
     let vectors = List.append rows cols
 
     let hasWinner =
         vectors
-        |> List.exists (fun v -> v |> List.forall (fun (pos, nb) -> nb.marked))
+        |> Seq.exists (fun v -> v |> Seq.forall (fun nb -> nb.marked))
 
     hasWinner
 
 let rec findLastWinningBoard numbers boards =
+    printfn "%A" numbers
+    printfn "%A" (boards |> Seq.length)
     let (n :: ns) = numbers
     let marked = boards |> List.map (mark n)
     let winner = marked |> List.tryFind isWinner
@@ -113,35 +95,21 @@ let solve text =
 
     let sumOfUnmarkedNumbers =
         winner
-        |> Map.values
+        |> Seq.cast<Number>
         |> Seq.filter (fun n -> n.marked |> not)
         |> Seq.map (fun n -> n.value)
         |> Seq.sum
 
     lastnumber * sumOfUnmarkedNumbers
 
-solve input
+solve example
 
-#r "nuget: Unquote"
-open Swensen.Unquote
+//#r "nuget: Unquote"
+//open Swensen.Unquote
 
-let run () =
-    printf "Testing..."
+//let run () =
+//    printf "Testing..."
 
-    test
-        <@ mark
-            3
-            ([ ({ row = 1; col = 2 }, { value = 3; marked = false }) ]
-             |> Map.ofSeq) = ([ ({ row = 1; col = 2 }, { value = 3; marked = true }) ]
-                              |> Map.ofSeq) @>
+//    printfn "...done!"
 
-    test
-        <@ mark
-            6
-            ([ ({ row = 1; col = 2 }, { value = 3; marked = false }) ]
-             |> Map.ofSeq) = ([ ({ row = 1; col = 2 }, { value = 3; marked = false }) ]
-                              |> Map.ofSeq) @>
-
-    printfn "...done!"
-
-run ()
+//run ()
