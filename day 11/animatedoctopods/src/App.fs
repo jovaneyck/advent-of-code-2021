3bd9ed6ba013adca29855e09bc38a7c30a117fe2
Van Eyck Jo <jo.vaneyck@gmail.com>
2021-12-13 16:23:32 +0100

    fully working elmish-react example with subscriptions

--- day 11/animatedoctopods/src/App.fs
+++ day 11/animatedoctopods/src/App.fs
@@ -1,24 +1,52 @@
 module App
 
+open System
 open Elmish
-type Msg = AMessage
 
-let init () = "Hello world from elmish-react"
-let update (msg: Msg) model = model + "-click"
+type Msg =
+    | ButtonClick
+    | Tick of DateTime
+
+type Model = { message: string; time: DateTime }
+
+let init () =
+    { message = "Hello world from elmish-react"
+      time = DateTime.Now },
+    Cmd.none
+
+let update (msg: Msg) model =
+    match msg with
+    | ButtonClick ->
+        { model with
+              message = model.message + "-clicked!" },
+        Cmd.none
+    | Tick timestamp -> { model with time = timestamp }, Cmd.none
 
 open Fable.React
 open Fable.React.Props
 
 let view model dispatch : ReactElement =
     div [] [
-        button [ OnClick(fun _ -> dispatch AMessage) ] [
+        str model.message
+        br []
+        str <| sprintf "%A" model.time
+        br []
+        button [ OnClick(fun _ -> dispatch ButtonClick) ] [
             str "clicky"
         ]
-        str model
     ]
 
 open Elmish.React
+open Browser
 
-Program.mkSimple init update view
+let subscription model =
+    let sub dispatch =
+        window.setInterval ((fun _ -> dispatch (Tick System.DateTime.Now)), 1000)
+        |> ignore
+
+    Cmd.ofSub sub
+
+Program.mkProgram init update view
 |> Program.withReactBatched "elmish-app"
+|> Program.withSubscription subscription
 |> Program.run
