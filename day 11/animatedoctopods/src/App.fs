531c81e38cfcb94934edea3bc7f0dabf08303f3d
Van Eyck Jo <jo.vaneyck@gmail.com>
2021-12-13 17:03:42 +0100

    elmish visualisation for day 11

--- day 11/animatedoctopods/src/App.fs
+++ day 11/animatedoctopods/src/App.fs
@@ -1,39 +1,98 @@
-module App
+﻿module App
+
+let example =
+    @"5483143223
+    2745854711
+    5264556173
+    6141336146
+    6357385478
+    4167524645
+    2176841721
+    6882881134
+    4846848554
+    5283751526"
+        .Split("\n")
+    |> Array.map (fun s -> s.Trim())
 
 open System
 open Elmish
 
-type Msg =
-    | ButtonClick
-    | Tick of DateTime
+type Msg = Tick of DateTime
 
-type Model = { message: string; time: DateTime }
+type Model =
+    { time: DateTime
+      generation: int
+      grid: Octopods.Grid }
 
 let init () =
-    { message = "Hello world from elmish-react"
-      time = DateTime.Now },
+    { time = DateTime.Now
+      generation = 0
+      grid = example |> Octopods.parse },
     Cmd.none
+
+let tickGrid model =
+    if model.grid |> Octopods.allFlashed then
+        model
+    else
+        { model with
+              generation = model.generation + 1
+              grid = Octopods.takeStep model.grid }
 
 let update (msg: Msg) model =
     match msg with
-    | ButtonClick ->
-        { model with
-              message = model.message + "-clicked!" },
-        Cmd.none
-    | Tick timestamp -> { model with time = timestamp }, Cmd.none
+    | Tick timestamp ->
+        model
+        |> tickGrid
+        |> fun m -> { m with time = timestamp }, Cmd.none
 
 open Fable.React
 open Fable.React.Props
 
+let renderGrid (g: Octopods.Grid) =
+    let maxX =
+        g
+        |> Map.toSeq
+        |> Seq.maxBy (fst >> fst)
+        |> fst
+        |> fst
+
+    let maxY =
+        g
+        |> Map.toSeq
+        |> Seq.maxBy (fst >> snd)
+        |> fst
+        |> snd
+
+    [ for x in 0 .. maxX do
+          yield!
+              [ for y in 0 .. maxY ->
+                    let energyLevel = g |> Map.find (x, y)
+
+                    span [ Style [ Color "yellow"
+                                   Opacity(
+                                       if energyLevel = 0 then
+                                           1m
+                                       else
+                                           decimal energyLevel / 10m
+                                   ) ] ] [
+                        str (energyLevel |> string)
+                    ] ]
+
+          yield br [] ]
+
 let view model dispatch : ReactElement =
     div [] [
-        str model.message
+        str "Animating octopods v2021.12.11"
+        br []
+        str "Made with fable, elmish and lots of #adventofcode ♥"
+        br []
+        str "Generation:"
+        str <| string model.generation
         br []
         str <| sprintf "%A" model.time
         br []
-        button [ OnClick(fun _ -> dispatch ButtonClick) ] [
-            str "clicky"
-        ]
+        br []
+        div [] (renderGrid model.grid)
     ]
 
 open Elmish.React
@@ -41,7 +100,7 @@ open Browser
 
 let subscription model =
     let sub dispatch =
-        window.setInterval ((fun _ -> dispatch (Tick System.DateTime.Now)), 1000)
+        window.setInterval ((fun _ -> dispatch (Tick System.DateTime.Now)), 100)
         |> ignore
 
     Cmd.ofSub sub
---
+++ day 11/animatedoctopods/src/Octopods.fs
@@ -0,0 +1,64 @@
+﻿module Octopods
+
+type Location = (int * int)
+type EnergyLevel = int
+type Grid = Map<Location, EnergyLevel>
+
+let parse (lines: string seq) : Grid =
+    [ for r, line in lines |> Seq.indexed do
+          for c, cell in line |> Seq.indexed -> ((r, c), cell |> string |> int) ]
+    |> Map.ofSeq
+
+let findNeighbours grid (r, c) =
+    [ (-1, -1)
+      (-1, 0)
+      (-1, 1)
+      (0, -1)
+      (0, 1)
+      (1, -1)
+      (1, 0)
+      (1, 1) ]
+    |> Seq.map (fun (dr, dc) -> (r + dr, c + dc))
+    |> Seq.filter (fun loc -> grid |> Map.containsKey loc)
+    |> Seq.toList
+
+let increaseEnergyLevelAt (grid: Grid) (loc: Location) : Grid =
+    let currentLevel = grid |> Map.find loc
+    grid |> Map.add loc (currentLevel + 1)
+
+let takeStep grid =
+    let rec resolveFlashes highlighted grid =
+        let nextOctupusToFlash =
+            grid
+            |> Map.toList
+            |> List.filter (fun (loc, level) -> highlighted |> Set.contains loc |> not)
+            |> List.tryFind (fun (_, v) -> v > 9)
+        //printfn "flashing: %A" nextOctupusToFlash
+        match nextOctupusToFlash with
+        | None ->
+            //Done with this step!
+            let resetGrid =
+                grid
+                |> Map.map
+                    (fun location level ->
+                        if highlighted |> Set.contains location then
+                            0
+                        else
+                            level)
+
+            resetGrid
+        | Some (loc, _) ->
+            //An octopus flashes!
+            let neighbours = findNeighbours grid loc
+
+            let gridWithIncreasedNeighbours =
+                neighbours |> List.fold increaseEnergyLevelAt grid
+
+            resolveFlashes (highlighted |> Set.add loc) gridWithIncreasedNeighbours
+
+    grid
+    |> Map.map (fun _ level -> level + 1)
+    |> resolveFlashes Set.empty
+
+let allFlashed (grid: Grid) =
+    grid |> Map.forall (fun _ level -> level = 0)
