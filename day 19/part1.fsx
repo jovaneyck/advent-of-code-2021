d89d37d4b93471835cc1998bceb4cb0f6168fd07
Van Eyck Jo <j.vaneyck@farmad.be>
2021-12-19 13:04:18 +0100

    playing with another idea, might be worth exploring but NOT ON MY WEEKEND LOLO

--- day 19/part1.fsx
+++ day 19/part1.fsx
@@ -1,64 +1,111 @@
 #r "nuget: Unquote"
 open Swensen.Unquote
 
-let input =
-    System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}\input.txt"
-
 let example =
-    @"".Split("\n") |> Array.map (fun s -> s.Trim())
+    System.IO.File.ReadAllText $"{__SOURCE_DIRECTORY__}\example.txt"
 
-let multiply ma mb =
-    let rowsA = ma |> Array2D.length1
-    let colsB = mb |> Array2D.length2
+let input =
+    System.IO.File.ReadAllText $"{__SOURCE_DIRECTORY__}\input.txt"
 
-    [ for i in 0 .. (rowsA - 1) do
-          [ for j in 0 .. (colsB - 1) do
-                let vectA = ma.[i, *]
-                let vectB = mb.[*, j]
+//Algorithm outline:
+//For every scanner:
+// * Grab a random beacon
+// * Calculate the distance from that beacon to every other beacon into a "distance map"
+//I think you can find "sensor overlaps" by just looking at these distance sets
 
-                Seq.zip vectA vectB
-                |> Seq.map (fun t -> fst t * snd t)
-                |> Seq.sum ] ]
-    |> array2D
+type Vector = int list
+type Scanner = { id: int; beacons: Vector list }
 
-let rotations v =
-    let sin =
-        [ 0, 0; 90, 1; 180, 0; 270, -1 ] |> Map.ofSeq
+let parseBeacon (text: string) : Vector =
+    let components = text.Split(",")
+    components |> Seq.map int |> Seq.toList
 
-    let cos =
-        [ 0, 1; 90, 0; 180, -1; 270, 0 ] |> Map.ofSeq
+let parseScanner (text: string seq) : Scanner =
+    let header = text |> Seq.head
 
-    let Rx t =
-        [ [ 1; 0; 0 ]
-          [ 0; cos.[t]; -sin.[t] ]
-          [ 0; sin.[t]; cos.[t] ] ]
-        |> array2D
+    let id =
+        header.Split("---", System.StringSplitOptions.RemoveEmptyEntries).[0]
+            .Trim()
+            .Split(
+            " "
+        ).[1]
+        |> int
 
-    let Ry t =
-        [ [ cos.[t]; 0; sin.[t] ]
-          [ 0; 1; 0 ]
-          [ -sin.[t]; 0; cos.[t] ] ]
-        |> array2D
+    let beacons =
+        text
+        |> Seq.tail
+        |> Seq.map parseBeacon
+        |> List.ofSeq
 
-    let Rz t =
-        [ [ cos.[t]; -sin.[t]; 0 ]
-          [ sin.[t]; cos.[t]; 0 ]
-          [ 0; 0; 1 ] ]
-        |> array2D
+    { id = id; beacons = beacons }
 
-    let rotationmatrices =
-        [ 0; 90; 180; 270 ]
-        |> List.collect (fun d -> [ Rx; Ry; Rz ] |> List.map (fun m -> m d))
-        |> List.distinct //Is it normal we have duplicates for the base rotations?
+let parse (input: string) =
+    input.Split("\r\n\r\n")
+    |> Seq.map (fun scanner -> scanner.Split("\n"))
+    |> Seq.map parseScanner
+    |> Seq.toList
 
-    rotationmatrices
-    |> List.map (fun R -> multiply v R)
+//Represents distances from the first beacon of a scanner to all the other beacons
+type DistanceMap = Set<int> list
 
-rotations (array2D [ [ 8; 0; 7 ] ])
+let distanceMap (scanner: Scanner) : DistanceMap =
+    let distance (one: Vector) (other: Vector) =
+        List.zip one other
+        |> List.map (fun (a, b) -> abs (a - b))
+        |> List.sum
+
+    [ for beacon in scanner.beacons do
+          let distances =
+              scanner.beacons
+              |> List.except [ beacon ]
+              |> List.map (distance beacon)
+              |> Set.ofList
+
+          distances ]
 
 let run () =
     printf "Testing..."
-    test <@ 1 + 1 = 2 @>
+
+    let small_single_scanner_input =
+        @"--- scanner 1337 ---
+        -1,-1,1
+        -2,-2,2
+        -3,-3,3
+        -2,-3,1
+        5,6,-4
+        8,0,7"
+            .Split("\n")
+        |> Array.map (fun s -> s.Trim())
+        |> String.concat "\n"
+
+    test
+        <@ parse small_single_scanner_input = [ { id = 1337
+                                                  beacons =
+                                                      [ [ -1; -1; 1 ]
+                                                        [ -2; -2; 2 ]
+                                                        [ -3; -3; 3 ]
+                                                        [ -2; -3; 1 ]
+                                                        [ 5; 6; -4 ]
+                                                        [ 8; 0; 7 ] ] } ] @>
+
     printfn "...done!"
 
 run ()
+
+let maps =
+    parse example
+    |> Seq.map (fun s -> s, distanceMap s)
+    |> Seq.toList
+
+let (_, map) = maps.[0]
+
+let connecting =
+    maps
+    |> List.tail
+    |> List.find
+        (fun (_, m) ->
+            m
+            |> List.exists
+                (fun m ->
+                    map
+                    |> List.exists (fun map -> (Set.intersect m map) |> Set.count >= 11)))
